a02b97154f63170a3d259b7ec7307cc1f80b145e
agga1 <agud100@gmail.com>
2020-01-16 16:53:51 +0100

    [ADD] registeredForTheDay trigger

--- Triggers.sql
+++ Triggers.sql
@@ -3,14 +3,36 @@ CREATE TRIGGER CheckForTwoTheSameConferenceDays
     ON Conference_days
     AFTER INSERT, UPDATE AS
 BEGIN
-    DECLARE @Date date = (SELECT date FROM inserted) --
+    DECLARE @Date date = (SELECT date FROM inserted)
     DECLARE @ConferenceId int = (SELECT conference_id FROM inserted)
     IF ((SELECT COUNT(conference_day_id)
          FROM Conference_days
-         WHERE (date = @Date) AND (conference_id = @ConferenceId)) > 1)
+         WHERE (date = @Date)
+           AND (conference_id = @ConferenceId)) > 1)
         BEGIN
             DECLARE @message varchar(100) = 'Day ' + CAST(@Date as varchar(3)) +
                                             ' has already been added for this conference';
             THROW 52000,@message,1 ROLLBACK TRANSACTION
         END
 END
+
+-- sprawdzenie czy uczestnik zapisujący się na warsztaty jest już zapisany na odpowiedni dzień konferencji
+CREATE TRIGGER CheckIfRegisteredForTheDay
+    ON Workshop_registration
+    AFTER INSERT, UPDATE AS
+BEGIN
+    DECLARE @ReservationId int = (SELECT reservation_id FROM inserted)
+    DECLARE @ParticipantID int = (SELECT participant_id FROM inserted)
+    DECLARE @ConferenceDayReservationID int = (SELECT Conference_day_reservations_reservation_id
+                                               FROM Workshop_reservations
+                                               WHERE Workshop_reservations.reservation_id = @ReservationId)
+    IF (@ParticipantID not in (SELECT participant_id
+                               FROM Conference_day_registration Cdr
+                               WHERE Cdr.reservation_id = @ConferenceDayReservationID))
+        BEGIN
+            DECLARE @message varchar(100) = 'Participant nr ' + CAST(@ParticipantID as varchar(3)) +
+                                            ' is not registered for the corresponding conference day';
+            THROW 52000,@message,1 ROLLBACK TRANSACTION
+        END
+END
+
