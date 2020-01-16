0ef5032c56e24a59cfa5ef1c22d544bb26cbda31
Maciej <maciek2tr99@gmail.com>
2020-01-17 00:19:48 +0100

    [Update] Clients balance as function

--- Functions.sql
+++ Functions.sql
@@ -61,6 +61,18 @@ BEGIN
 END
 go
 
+CREATE FUNCTION checkClientsBalance(@ClientId int)
+    RETURNS MONEY
+AS BEGIN
+    DECLARE @paid MONEY = (SELECT SUM(P.VALUE) FROM Payments P
+                                inner join Conference_day_reservations Cdr on P.reservation_id = Cdr.reservation_id
+        WHERE Cdr.clients_id = @ClientId)
+    DECLARE @owed MONEY  = (SELECT SUM(dbo.conferenceReservationPrice(cdr.reservation_id)) FROM Conference_day_reservations cdr
+                                WHERE cdr.clients_id = @ClientId)
+
+    RETURN @paid - @owed
+END
+
 /*CREATE FUNCTION getPaidAmount(@reservationId int)
     RETURNS INT
 AS
