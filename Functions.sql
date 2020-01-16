f378c533d2b1513411c5d8613fe1695ca9b538e8
agga1 <agud100@gmail.com>
2020-01-16 20:07:09 +0100

    [Add] price calculating functions

--- Functions.sql
+++ Functions.sql
@@ -1,9 +1,31 @@
-create function getPaidAmount(@reservationId int)
-returns int
-as begin
-    return (select sum(p.value) from Payments p where p.reservation_id=@reservationId group by p.reservation_id);
-end
-go
+CREATE FUNCTION getPaidAmount(@reservationId int)
+RETURNS INT
+AS BEGIN
+    RETURN (SELECT sum(p.value) FROM Payments p WHERE p.reservation_id=@reservationId GROUP BY p.reservation_id);
+END
+GO
+
+CREATE FUNCTION calcExpectedPriceForConferenceDay(@reservationId int)
+RETURNS INT
+AS BEGIN
+    DECLARE @nrOfStudents int = (SELECT student_seats FROM Conference_day_reservations WHERE reservation_id = @reservationId)
+    DECLARE @nrOfAdults int = (SELECT adult_seats FROM Conference_day_reservations WHERE reservation_id = @reservationId)
+    DECLARE @conferenceDayID int = (SELECT conference_day_id FROM Conference_day_reservations WHERE reservation_id = @reservationId)
+    DECLARE @standardPrice money = (SELECT standard_price FROM Conference_days WHERE conference_day_id = @conferenceDayID)
+    DECLARE @studentDiscount decimal(4,4) = (SELECT student_discount FROM Conference_days WHERE conference_day_id = @conferenceDayID)
+    RETURN @nrOfAdults*@standardPrice + @nrOfStudents*(@standardPrice*(1+@studentDiscount)) -- TODO assuming discount passed as negative value
+END
+GO
+
+
+CREATE FUNCTION calcPriceForWorkshopReservation(@reservationId int)
+RETURNS INT
+AS BEGIN
+    DECLARE @workshopID int = (SELECT workshop_id FROM Workshop_reservations WHERE reservation_id = @reservationId)
+    DECLARE @price money = (SELECT price FROM Workshops WHERE workshop_id = @workshopID)
+    RETURN @price*(SELECT nr_of_seats FROM Workshop_reservations WHERE reservation_id = @reservationId)
+END
+GO
 
 CREATE FUNCTION getWorkshopsFreeSeatsCount(@workshopId int)
 RETURNS INT
