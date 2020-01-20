1597f180d7b617d0ae94a58f9b8fa133f2020b1d
Maciej <maciek2tr99@gmail.com>
2020-01-20 20:01:57 +0100

    [Update] Addition Procedures

--- Procedures/AdditionProcedures.sql
+++ Procedures/AdditionProcedures.sql
@@ -1,27 +1,4 @@
-CREATE PROCEDURE AddConference -- TODO add conference location? start-end date?
-    @Name varchar(100)
-AS
-BEGIN
-    SET NOCOUNT ON;
-    INSERT INTO Conferences(Name)
-    VALUES (@Name)
-END
-GO
-
-CREATE PROCEDURE AddConferenceDay @ConferenceId int,
-                                  @Date date,
-                                  @StandardPrice money,
-                                  @StudentDiscount int
-AS
-BEGIN
-    SET NOCOUNT ON;
-    INSERT INTO Conference_days
-        (conference_id, date, standard_price, student_discount)
-    VALUES (@ConferenceId, @Date, @StandardPrice, @StudentDiscount)
-END
-GO
-
--- Add client (legal person) to database
+-- Add client to database
 CREATE PROCEDURE AddClientParticipant @ZipCode varchar(6), @City varchar(30), @Address varchar(100), @Name varchar(100),
                                       @Surname varchar(100),
                                       @Email varchar(100), @Phone varchar(20)
@@ -49,18 +26,7 @@ BEGIN
 END
 GO
 
--- Add company (each has its unique clients_id)
--- CREATE PROCEDURE AddCompany @companyName varchar(100), @nip nvarchar(15), @phone varchar(20),
---                             @clients_id int, @email varchar(100)
--- AS
--- BEGIN
---     SET NOCOUNT ON;
---     INSERT INTO Companies(companyName, nip, phone, clients_id, email)
---     VALUES (@companyName, @nip, @phone, @clients_id, @email)
--- END
--- GO
-
--- Add participant (natural person)
+-- Add participant
 CREATE PROCEDURE AddParticipant @Name varchar(100), @Surname varchar(100),
                                 @Email varchar(100), @Phone varchar(20)
 AS
@@ -80,26 +46,115 @@ BEGIN
 END
 GO
 
-create procedure AddEarlySignupDiscount @ConferenceDayId int, @EndDate datetime,
-                                        @Discount decimal(4, 4)
-as
-begin
-    set nocount on;
-    insert into Early_signup_discounts(conference_day_id, end_date, discount)
-    values (@ConferenceDayId, @EndDate, @Discount)
-end
-go
+CREATE PROCEDURE AddConference @Name varchar(100),
+                               @description text = null
+AS
+BEGIN
+    SET NOCOUNT ON;
+    INSERT INTO Conferences(name, description)
+    VALUES (@Name, @description)
+END
+GO
 
-create procedure AddWorkshop @conference_day_id int,
-                             @start_time datetime, @end_time datetime,
-                             @topic text, @price money, @number_of_seats int
-as
-begin
+CREATE PROCEDURE AddConferenceDay @ConferenceId int,
+                                  @Date date,
+                                  @StandardPrice money,
+                                  @StudentDiscount int,
+                                  @NumberOfSeats int
+AS
+BEGIN
+    SET NOCOUNT ON;
+    INSERT INTO Conference_days
+    (conference_id, date, standard_price, student_discount, number_of_seats)
+    VALUES (@ConferenceId, @Date, @StandardPrice, @StudentDiscount, @NumberOfSeats)
+END
+GO
+
+CREATE PROCEDURE AddConferenceWithDays @Name varchar(100),
+                                       @numberOfDays int,
+                                       @startDate date,
+                                       @price money,
+                                       @studentDiscount decimal(4, 4),
+                                       @numberOfSeats int,
+                                       @description text = null
+AS
+BEGIN
+    SET NOCOUNT ON;
+    INSERT INTO Conferences(name, description)
+    VALUES (@Name, @description)
+
+    DECLARE @confId int = SCOPE_IDENTITY()
+
+    WHILE @numberOfDays > 0
+        BEGIN
+            EXEC AddConferenceDay
+                 @confId,
+                 @startDate,
+                 @price,
+                 @studentDiscount,
+                 @numberOfSeats;
+            SET @startDate = DATEADD(DAY, 1, @startDate)
+            SET @numberOfSeats = @numberOfSeats - 1
+        END
+END
+GO
+
+CREATE PROCEDURE AddEarlySignupDiscount @ConferenceDayId int,
+                                        @EndDate datetime,
+                                        @Discount decimal(4, 4)
+AS
+BEGIN
+    SET NOCOUNT ON;
+    INSERT INTO Early_signup_discounts(conference_day_id, end_date, discount)
+    VALUES (@ConferenceDayId, @EndDate, @Discount)
+END
+GO
+
+CREATE PROCEDURE AddEarlySignupDiscountToAllInConf @ConferenceId int,
+                                                   @EndDate datetime,
+                                                   @Discount decimal(4, 4)
+AS
+BEGIN
+    SET NOCOUNT ON;
+    CREATE TABLE #ids
+    (
+        rn int,
+        id int
+    )
+    INSERT INTO #ids
+    SELECT DISTINCT row_number() over (order by conference_day_id) as rn, conference_day_id as id
+    FROM Conference_days
+    WHERE conference_id = @ConferenceId;
+
+    DECLARE @id int
+    DECLARE @totalrows int = (select count(*) from #ids)
+    DECLARE @currentrow int = 1
+
+    WHILE @currentrow <= @totalrows
+        BEGIN
+            set @id = (select id from #ids where rn = @currentrow)
+            exec AddEarlySignupDiscount
+                 @id,
+                 @EndDate,
+                 @Discount
+            set @currentrow = @currentrow + 1
+        END
+END
+GO
+
+CREATE PROCEDURE AddWorkshop @conference_day_id int,
+                             @start_time datetime,
+                             @end_time datetime,
+                             @topic varchar(100),
+                             @price money,
+                             @number_of_seats int
+AS
+BEGIN
     set nocount on;
     insert into Workshops(conference_day_id, start_time, end_time, topic, price, number_of_seats)
     values (@conference_day_id, @start_time, @end_time, @topic, @price, @number_of_seats)
-end
-go
+END
+GO
 
 
 create procedure RegisterParticipantForConferenceDay @reservation_id int, @Participant_id int, @is_student bit
@@ -120,25 +175,29 @@ begin
 end
 go
 
-create procedure AddConferenceDayReservation @conference_day_id int, @clients_id int, @reservation_date datetime,
-                                             @active bit, @due_price datetime,
-                                             @adult_seats int, @student_seats int
+create procedure AddConferenceDayReservation @conference_day_id int,
+                                             @clients_id int,
+                                             @reservation_date datetime,
+                                             @due_price datetime,
+                                             @adult_seats int,
+                                             @student_seats int
 as
 begin
     set nocount on;
-    insert into Conference_day_reservations(conference_day_id, clients_id, reservation_date, active, due_price,
+    insert into Conference_day_reservations(conference_day_id, clients_id, reservation_date, due_price,
                                             adult_seats, student_seats)
-    values (@conference_day_id, @clients_id, @reservation_date, @active, @due_price, @adult_seats, @student_seats)
+    values (@conference_day_id, @clients_id, @reservation_date, @due_price, @adult_seats, @student_seats)
 end
 go
 
-create procedure AddWorkshopReservation @workshop_id int, @reservation_date datetime, @due_price datetime,
-                                        @nr_of_seats int
+create procedure AddWorkshopReservation @workshop_id int,
+                                        @conf_reservation_id int,
+                                        @nr_of_seats int,
+                                        @weeks_to_pay int
 as
 begin
     set nocount on;
-    insert into Workshop_reservations(workshop_id, reservation_date, due_price, nr_of_seats)
-    values (@workshop_id, @reservation_date, @due_price, @nr_of_seats)
+    insert into Workshop_reservations(workshop_id, Conference_day_res_id, nr_of_seats, due_price)
+    values (@workshop_id, @conf_reservation_id, @nr_of_seats, DATEADD(week, @weeks_to_pay, GETDATE()))
 end
 go
-
